f9530e6062eae4f7593d145d535a1c92f3cd944d
Zac Braddy <zacharybraddy@gmail.com>
2017-03-18 22:46:42 +0000

    Made a change to allow for multiple files to be aggregated into a single request to the server

--- app/BL/doLint.js
+++ app/BL/doLint.js
@@ -3,55 +3,73 @@ const linter = require('eslint').linter;
 const serverUrl = 'http://abstractsausagefactory.azurewebsites.net/api/tracking/';
 
 module.exports = (req) => {
-  const fileUrl = req.fileUrl;
+  const payload = {
+    UserName: '',
+    Xp: 0,
+    integrationsProvider: 100
+  };
 
-  console.log(`Alright bro, I'm just gonna go get ${fileUrl} real quick`);
+  const gitHubGets = [];
 
-  request.get(fileUrl, (error, response, body) => {
-    if (error || response.statusCode >= 400) {
-      throw new Error(`Oh crap son! We got an error on our hands getting to github man!\r\n${JSON.stringify(response)}`);
-    }
+  req.forEach((file) => {
+    const fileUrl = file.fileUrl;
+    console.log(`Alright bro, I'm just gonna go get ${fileUrl} real quick`);
 
-    console.log(`Sweet as a nut, I got ${fileUrl}`);
-    console.log(`Let's see what ESLint thinks of this bad boy`);
-
-    const modCount = req.modCount;
-    const username = req.username;
-
-    const fileNameParts = fileUrl.split('/');
-    const fileName = fileNameParts[fileNameParts.length - 1];
-
-    const messages = linter.verify(body, {
-      rules: {
-        semi: 2,
-      }
-    }, {filename: fileName});
-
-    if (messages.length === 0) {
-      console.log(`Dude, this file was sick! Sending points to the app server`);
-      console.log(`username: ${username}`);
-      console.log(`Xp: ${modCount}`);
-      console.log(`integrationsProvider: 100`);
-      request(
-        {
-          url: serverUrl,
-          method: 'PUT',
-          headers: {
-            'Content-type': 'application/json'
-          },
-          body: JSON.stringify({
-            UserName: username,
-            Xp: modCount,
-            integrationsProvider: 100
-          })
-        },
-        (errorFromAppServer, responseFromAppServer) => {
-          if (errorFromAppServer || responseFromAppServer.statusCode >= 400) {
-            throw new Error(`Oh crap son! We got an error on our hands talking to the app server man!\r\n${JSON.stringify(responseFromAppServer)}`);
+    gitHubGets.push(
+      new Promise((resolve) => {
+        request.get(fileUrl, (error, response, body) => {
+          if (error || response.statusCode >= 400) {
+            console.error(`Oh crap son! We got an error on our hands getting to github man!\r\n${JSON.stringify(response)}`);
+            resolve();
+            return;
           }
 
-          console.log(responseFromAppServer.statusCode);
+          console.log(`Sweet as a nut, I got ${fileUrl}`);
+          console.log(`Let's see what ESLint thinks of this bad boy`);
+
+          const fileNameParts = fileUrl.split('/');
+          const fileName = fileNameParts[fileNameParts.length - 1];
+
+          const messages = linter.verify(body, {
+            rules: {
+              semi: 2,
+            }
+          }, {filename: fileName});
+
+          if (messages.length === 0) {
+            payload.UserName = file.username;
+            const xpDiff = parseInt(file.modCount);
+            payload.Xp += !isNaN(xpDiff) ? xpDiff : 0;
+          }
+
+          resolve();
         });
-    }
+      })
+    );
+  });
+
+  Promise.all(gitHubGets).then(() => {
+    console.log(`Dude, these files were sick! Sending points to the app server`);
+    console.log(`username: ${payload.UserName}`);
+    console.log(`Xp: ${payload.Xp}`);
+    console.log(`integrationsProvider: 100`);
+
+    request(
+      {
+        url: serverUrl,
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+      },
+      (errorFromAppServer, responseFromAppServer) => {
+        if (errorFromAppServer || responseFromAppServer.statusCode >= 400) {
+          throw new Error(`Oh crap son! We got an error on our hands talking to the app server man!\r\n${JSON.stringify(responseFromAppServer)}`);
+        }
+
+        console.log(responseFromAppServer.statusCode);
+      }
+    );
   });
 };
