2b98997f94ddc9f5d16be399f3d4b782d3683a1e
Zac Braddy <zacharybraddy@gmail.com>
2017-03-18 19:53:08 +0000

    Changed back from fetch to request

--- app/BL/doLint.js
+++ app/BL/doLint.js
@@ -1,22 +1,17 @@
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
-
+const request = require('request');
 const linter = require('eslint').linter;
 const serverUrl = 'http://abstractsausagefactory.azurewebsites.net/api/tracking/';
 
 module.exports = (req) => {
-  console.log(req);
   const fileUrl = req.fileUrl;
 
   console.log(`Alright bro, I'm just gonna go get ${fileUrl} real quick`);
-  fetch(fileUrl).then((response) => {
-    if (response.status >= 400) {
-      throw new Error(`Oh crap son! We got an error on our hands man!\r\n${JSON.stringify(response)}`);
+
+  request.get(fileUrl, (error, response, body) => {
+    if (error || response.statusCode >= 400) {
+      throw new Error(`Oh crap son! We got an error on our hands getting to github man!\r\n${JSON.stringify(response)}`);
     }
 
-    return response.text();
-  })
-  .then((body) => {
     console.log(`Sweet as a nut, I got ${fileUrl}`);
     console.log(`Let's see what ESLint thinks of this bad boy`);
 
@@ -30,29 +25,30 @@ module.exports = (req) => {
       rules: {
         semi: 2,
       }
-    }, { filename: fileName});
+    }, {filename: fileName});
 
-    if (messages.length === 0) {
+    if (messages.length !== 0) {
       console.log(`Dude, this file was sick! Sending points to the app server`);
-      /*fetch(serverUrl, {
-        method: 'PUT',
-        headers: {
-          'Content-type': 'application/json'
+      request(
+        {
+          url: serverUrl,
+          method: 'PUT',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            UserName: username,
+            Xp: modCount,
+            integrationsProvider: 100
+          })
         },
-        body: JSON.stringify({
-          UserName: username,
-          Xp: modCount
-        })
-      }).catch(function(error) {
-        console.log('OHH SNAP!!! We got problems sending to the app server son!', error);
-      });*/
+        () => {
+          if (error || response.statusCode >= 400) {
+            throw new Error(`Oh crap son! We got an error on our hands talking to the app server man!\r\n${JSON.stringify(response)}`);
+          }
 
-      return;
+          console.log(response.statusCode);
+        });
     }
-
-    console.log(`You done messed up son. No points for you.`);
-  }).catch(function(error) {
-    console.log('OHH SNAP!!! We got problems getting to github son!', error);
   });
 };
-
