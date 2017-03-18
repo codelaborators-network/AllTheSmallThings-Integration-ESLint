307889ec81239b6199c7e8648a39db87b1806ad8
Zac Braddy <zacharybraddy@gmail.com>
2017-03-18 18:41:00 +0000

    Changed to isomporhic-fetch for requests and also hooked up the connection to the app server

--- app/BL/doLint.js
+++ app/BL/doLint.js
@@ -1,18 +1,27 @@
-const request = require('request');
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
 const linter = require('eslint').linter;
+const serverUrl = 'http://abstractsausagefactory.azurewebsites.net/api/tracking/';
 
 module.exports = (req) => {
+  console.log(req);
   const fileUrl = req.fileUrl;
-  const modCount = req.modCount;
 
   console.log(`Alright bro, I'm just gonna go get ${fileUrl} real quick`);
-  request.get(fileUrl, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
+  fetch(fileUrl).then((response) => {
+    if (response.status >= 400) {
       throw new Error(`Oh crap son! We got an error on our hands man!\r\n${JSON.stringify(response)}`);
     }
 
+    return response.text();
+  })
+  .then((body) => {
     console.log(`Sweet as a nut, I got ${fileUrl}`);
     console.log(`Let's see what ESLint thinks of this bad boy`);
+
+    const modCount = req.modCount;
+    const username = req.username;
 
     const fileNameParts = fileUrl.split('/');
     const fileName = fileNameParts[fileNameParts.length - 1];
@@ -23,7 +32,27 @@ module.exports = (req) => {
       }
     }, { filename: fileName});
 
-    console.log(messages);
+    if (messages.length === 0) {
+      console.log(`Dude, this file was sick! Sending points to the app server`);
+      fetch(serverUrl, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          UserName: username,
+          Xp: modCount
+        })
+      }).catch(function(error) {
+        console.log('OHH SNAP!!! We got problems sending to the app server son!', error);
+      });
+
+      return;
+    }
+
+    console.log(`You done messed up son. No points for you.`);
+  }).catch(function(error) {
+    console.log('OHH SNAP!!! We got problems getting to github son!', error);
   });
 };
 
